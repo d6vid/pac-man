82813407dbd6e7c91bfd7a0b4f7bb966f3f07870
David <davidcotosolano@outlook.com>
2024-10-26 13:44:27 -0600

    Adding game.py file and updating requirements.txt

---
+++ src/game.py
@@ -0,0 +1,12 @@
+class Game:
+    """
+    A class representing the game's state. It contains everything related to the field, pac-man (the player),
+    ghosts, collectibles and the general configuration.
+
+    Methods:
+    start_game(): Starts a game based on a pre-set configuration.
+
+    Authors:
+        David Coto, Lucía Somarribas, Valeria Marín
+    """
+    pass
